e99154d27255694ac603333cfa76ae7d05cbfaa2
Renato Magnus <rmagnusdarosa@gmail.com>
2024-09-13 22:47:42 -0300

    feat: search bar

--- client/src/components/searchBar/searchBar.jsx
+++ client/src/components/searchBar/searchBar.jsx
@@ -1,9 +1,54 @@
-import './styles.scss';
+import "./styles.scss";
+import { useState } from 'react';
 
+const types = ["Buy", "Rent"];
 const SearchBar = () => {
-  return (
-    <div className='searchBar'>SearchBar</div>
-  )
-}
+  const [query, setQuery] = useState({
+    type: "Buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
 
-export default SearchBar
+  const switchType = value => {
+    setQuery(prev => ({ ...prev, type: value }));
+  };
+
+  return (
+    <div className="search-bar">
+      <div className="type">
+        {types.map(type => (
+          <button key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+
+      <form>
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="text"
+          name="minPrice"
+          min={0}
+          max={10000000}
+          placeholder="Min Price"
+        />
+        <input
+          type="text"
+          name="maxPrice"
+          min={0}
+          max={10000000}
+          placeholder="Max Price"
+        />
+      <button>
+        <img src="/search.png" alt="" />
+      </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
