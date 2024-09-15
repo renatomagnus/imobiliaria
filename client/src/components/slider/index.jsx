0f0227759b66e542d39b63c764882ad78a85adb8
Renato Magnus <rmagnusdarosa@gmail.com>
2024-09-15 14:08:37 -0300

    feat: single-page pt 1

---
+++ client/src/components/slider/index.jsx
@@ -0,0 +1,9 @@
+import './styles.scss';
+
+const Slider = () => {
+  return (
+    <div className='slider'>Slider</div>
+  )
+}
+
+export default Slider
--- client/src/routes/singlePage/index.jsx
+++ client/src/routes/singlePage/index.jsx
@@ -1,9 +1,39 @@
-import './styles.scss';
+import Slider from "../../components/slider";
+import "./styles.scss";
+import { singlePostData, userData } from "../../lib/dummydata"
 
 const SinglePage = () => {
   return (
-    <div className=''>SinglePage</div>
-  )
-}
+    <div className="single-page">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images = {singlePostData.images}/>
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">
+              {singlePostData.description}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
+};
 
-export default SinglePage
+export default SinglePage;
