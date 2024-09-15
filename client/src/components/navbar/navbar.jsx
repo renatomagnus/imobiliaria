59d22e8762831b4b5fbd16aeb90cd171aac9ba0a
Renato Magnus <rmagnusdarosa@gmail.com>
2024-09-14 23:39:36 -0300

    feat: card

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,36 +1,37 @@
 import { useState } from "react";
 import "./styles.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [ open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   return (
     <nav>
       <div className="left">
-        <a href="/" className="logo">
+        <Link to="/" className="logo">
           <img src="/logo.png" alt="Logo" />
           <span>Imobiliária</span>
-        </a>
-        <a href="">Home</a>
-        <a href="">Sobre nós</a>
-        <a href="">Contato</a>
-        <a href="">Corretores</a>
+        </Link>
+        <Link to="">Home</Link>
+        <Link to="">Sobre nós</Link>
+        <Link to="">Contato</Link>
+        <Link to="">Corretores</Link>
       </div>
       <div className="right">
-        <a href="">Entrar</a>
-        <a href="" className="register">
+        <Link to="">Entrar</Link>
+        <Link to="" className="register">
           Cadastrar
-        </a>
+        </Link>
         <div className="menu-icon">
-          <img src="menu.png" alt="" onClick={() => setOpen(!open)}/>
+          <img src="menu.png" alt="" onClick={() => setOpen(!open)} />
         </div>
         <div className={open ? "menu active" : "menu"}>
-        <a href="">Home</a>
-        <a href="">Sobre nós</a>
-        <a href="">Contato</a>
-        <a href="">Corretores</a>
-        <a href="">Entrar</a>
-        <a href="">Cadastrar</a>
+          <Link to="">Home</Link>
+          <Link to="">Sobre nós</Link>
+          <Link to="">Contato</Link>
+          <Link to="">Corretores</Link>
+          <Link to="">Entrar</Link>
+          <Link to="">Cadastrar</Link>
         </div>
       </div>
     </nav>
