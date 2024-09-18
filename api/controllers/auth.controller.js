d6fca8de39bdb0b74359995cde3aaf00adbab709
Renato Magnus <rmagnusdarosa@gmail.com>
2024-09-18 00:24:41 -0300

    feat: start config routes

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  
+  
+  const hashedPassword = await bcrypt.hash(password, 10)
+  console.log(hashedPassword)
+}
+
+export const login = (req, res) => {
+  console.log('login')
+  
+}
+
+export const logout = (req, res) => {
+  console.log('1')
+
+}
