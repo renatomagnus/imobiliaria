765cbb1a367b49527ac66eced5bd1340b8d7f99d
Renato Magnus <rmagnusdarosa@gmail.com>
2024-09-22 00:06:21 -0300

    feat: prisma + create user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,30 @@
 import bcrypt from "bcrypt";
+import Prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
-  
-  
-  const hashedPassword = await bcrypt.hash(password, 10)
-  console.log(hashedPassword)
-}
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await Prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({ message: "User created succesfully!" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to create user" });
+  }
+};
 
 export const login = (req, res) => {
-  console.log('login')
-  
-}
+  console.log("login");
+};
 
 export const logout = (req, res) => {
-  console.log('1')
-
-}
+  console.log("1");
+};
