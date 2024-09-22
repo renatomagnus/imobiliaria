f9e0f1544e3a3bfa940178255f79981f8dbcbc8e
Renato Magnus <rmagnusdarosa@gmail.com>
2024-09-22 00:45:48 -0300

    feat [backend]: logout

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -55,5 +55,5 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  console.log("1");
+  res.clearCookie("token").status(200).json({ message: "Logout Successful"})
 };
