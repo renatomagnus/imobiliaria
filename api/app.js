4abc1b0e5757bed3fac8b7ae6446bacf89603676
Renato Magnus <rmagnusdarosa@gmail.com>
2024-09-22 22:22:37 -0300

    feat [backend]: authorization

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";  
 import dotenv from "dotenv";
@@ -15,6 +16,7 @@ app.use(cookieParser())
 
 app.use("/api/posts", postRoute)
 app.use("/api/auth", authRoute)
+app.use("/api/test", testRoute)
 
 app.listen(3002, () => {
   console.log("server is running!");
